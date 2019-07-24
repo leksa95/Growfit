3637e4008c9e49b530a3af9d0810800de972b070
Aleksandra <a.shvets@civenty.com>
2019-07-24 15:47:44 +0300

    new version 02

--- src/js/index.js
+++ src/js/index.js
@@ -26,95 +26,66 @@ $('.video_block .text_wrap, .section .text_wrap').each(function(){
         .addTo(controller);
 });
 
-
-// BLUE LINE
-var wipeAnimation = new TimelineMax()
-    .to(".section-two", 1,   {
-        x: "0%"
-    });
+//SHOW LINE
+var showLine = new TimelineMax();
+showLine.to(".line", 1, {width: "30vw", opacity: 1}, "+=2")
 new ScrollMagic.Scene({
-    triggerElement: ".section-two",
+    triggerElement: ".top_block",
+    duration: 1000,
+    triggerHook: 0.5
 
-    triggerHook: "onLeave",
-    duration: "100%",
-    ease: Linear.easeNone
 })
-    .setPin(".section-two")
-    .setTween(wipeAnimation)
+    .setTween(showLine)
     // .addIndicators()
+    // .setPin(".video_block")
     .addTo(controller);
+
+//HIDE LINE
+var hideLine = new TimelineMax();
+hideLine.to(".line", 1, {width: "0", opacity: 0},"+=3")
+new ScrollMagic.Scene({
+    triggerElement: ".video_block",
+    duration: 200,
+    triggerHook: 0
+
+})
+    .setTween(hideLine)
+    .addIndicators()
+    // .setPin(".video_block")
+    .addTo(controller);
+
 
 //VIDEO SHOW
-var moveImg = new TimelineMax();
-moveImg
-    .fromTo(".video_wrap", 1, {xPercent: 0, autoAlpha: 0, width: "30vw", opacity: 1}, {xPercent: 0, autoAlpha: 1, width: "30vw", opacity: 0, ease: Power4.easeInOut})
+var moveVideo = new TimelineMax();
+moveVideo.to(".video_wrap", 1, { opacity: 1})
+         .to(".video_wrap", 1, { width: 0}, "+=1")
+         .to(".section", 2, { width: "100%"}, "+=3")
+
 new ScrollMagic.Scene({
-    triggerElement: "#trigger1",
-    duration: '100%'
+    triggerElement: ".video_block",
+    duration: 1000,
+    triggerHook: 0
+
 })
-    .setTween(moveImg)
-    .addTo(controller);
-
-
-//VIDEO REMOVE
-var removeImg = new TimelineMax();
-removeImg
-    .fromTo(".section-two", 1, {xPercent: 0, autoAlpha: 1, width: "30vw", opacity: 1}, {xPercent: 0, autoAlpha: 1, width: "0", opacity: 0})
-    .fromTo(".video_wrap", 1, {xPercent: 0, autoAlpha: 1, width: "30vw"}, {xPercent: 0, autoAlpha: 1, width: "0"})
-    .fromTo(".section", 1, {xPercent: 0, autoAlpha: 1, width: "0", opacity: 0}, {xPercent: 0, autoAlpha: 1, width: "100%", opacity: 1})
-new ScrollMagic.Scene({
-    triggerElement: "#trigger3",
-    duration: '30%'
-})
-    .setTween(removeImg)
+    .setTween(moveVideo)
+    // .addIndicators()
+    .setPin(".video_block")
     .addTo(controller);
 
 
 
+// var showBlock = new TimelineMax();
+// showBlock.to(".section", 2, { opacity: 1, width: "100%"}, "+=20")
+// new ScrollMagic.Scene({
+//     triggerElement: ".video_block",
+//     duration: 1000,
+//     triggerHook: 1
+//
+// })
+//     .setTween(showBlock)
+//     // .addIndicators()
+//     // .setPin(".video_block")
+//     .addTo(controller);
+//
 
-var flightpath = {
-    entry : {
-        curviness: 1.25,
-        autoRotate: true,
-        values: [
-            {x: 100,	y: -100},
-            {x: 300,	y: 10}
-        ]
-    },
-    looping : {
-        curviness: 1.25,
-        autoRotate: true,
-        values: [
-            {x: 510,	y: 60},
-            {x: 620,	y: -60},
-            {x: 500,	y: -100},
-            {x: 380,	y: 20},
-            {x: 500,	y: 60},
-            {x: 580,	y: 20},
-            {x: 620,	y: 15}
-        ]
-    },
-    leave : {
-        curviness: 1.25,
-        autoRotate: true,
-        values: [
-            {x: 660,	y: 20},
-            {x: 800,	y: 130},
-            {x: $(window).width() + 300,	y: -100},
-        ]
-    }
-};
-
-// create tween
-var tween2 = new TimelineMax()
-    .add(TweenMax.to($("#plane"), 0, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}))
-    .add(TweenMax.to($("#plane"), 0, {css:{bezier:flightpath.looping}, ease:Power1.easeInOut}))
-    .add(TweenMax.to($("#plane"), 100, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}));
-
-
-// build scene
-var scene4 = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 500, offset: 100})
-    // .setPin("#target")
-    .setTween(tween2)
-    .addTo(controller);
 
